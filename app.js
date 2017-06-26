fa06eaf3281fc5e395a879272b4c00fa17b3d03f
jhackGC <JhackGitHub83_>
2017-06-26 23:10:30 +1000

    Adds data to the content api

---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ * Created by javierhack on 25/6/17.
+ */
+var express = require('express');
+var logoData = require('./data/logo-data.json');
+var bannerData = require('./data/banner-data.json');
+var menuData = require('./data/menu-data.json');
+var app = new express();
+var cors = require('cors');
+var _  = require('lodash');
+
+app.use(function(req, res, next) {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+// app.use(express.static('./public')); our client is another application
+
+// apply CORS middleware to allow requests from any domain
+app.use(cors());
+
+app.get('/content-api', function(req, res) {
+  const data = _.merge(logoData, bannerData, menuData);
+  res.json(data);
+});
+
+app.listen(3002);
+
+console.log('SouthSwell express server app running in port 3002');
+
+module.exports = app;
