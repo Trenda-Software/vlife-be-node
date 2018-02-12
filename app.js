a86baa618a668f2d7c2cbf7df597b54067201ea3
javierhack@gmail.com <JhackBitBucket83_>
2018-02-12 22:18:18 +1000

    set up

--- app.js
+++ app.js
@@ -2,17 +2,14 @@
  * Created by javierhack on 25/6/17.
  */
 var express = require('express');
-var logoData = require('./data/logo-data.json');
-var bannerData = require('./data/banner-data.json');
-var menuData = require('./data/menu-data.json');
-var servicesData = require('./data/services-data.json');
+var cucaSiteData = require('./data/cuca/siteData.json');
 var app = new express();
 var cors = require('cors');
-var _  = require('lodash');
+// var _ = require('lodash');
 
-app.use(function(req, res, next) {
-  console.log(`${req.method} request for '${req.url}'`);
-  next();
+app.use(function (req, res, next) {
+    console.log(`${req.method} request for '${req.url}`);
+    next();
 });
 
 // app.use(express.static('./public')); our client is another application
@@ -20,9 +17,10 @@ app.use(function(req, res, next) {
 // apply CORS middleware to allow requests from any domain
 app.use(cors());
 
-app.get('/content-api', function(req, res) {
-  const data = _.merge(logoData, bannerData, menuData, servicesData);
-  res.json(data);
+app.get('/cuca/data', function (req, res) {
+    // const data = _.merge(logoData, bannerData, menuData, servicesData);
+    const data = cucaSiteData;
+    res.json(data);
 });
 
 app.listen(3002);
