51fe717d15479221c00cf8904536a8ac39170644
jhackGC <javierhack@gmail.com>
2019-09-12 16:56:07 -0300

    adds CircleCI

--- src/routes/pageRoutes.ts
+++ src/routes/pageRoutes.ts
@@ -3,10 +3,7 @@ import DataService from '../service/DataService';
 const pageRoutes = (app: any, ds: DataService) => {
     app.route('/:site/page')
         .get(async (req: any, res: any) => {
-            // console.log(req.params);
             const pages = await ds.getPages(req.params.site);
-            console.log('route pages: ', pages);
-
             res.send(pages);
         })
         .post(async (req: any, res: any) => res.send('page POST Req Ok'));
