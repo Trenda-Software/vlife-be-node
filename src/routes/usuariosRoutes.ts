e7801abe91e23f7da67ca0dcc3fa6cd3d67defd0
jhackGC <javierhack@gmail.com>
2019-12-10 18:43:41 -0300

    infla asociaciones para mostrarlas

--- src/routes/usuariosRoutes.ts
+++ src/routes/usuariosRoutes.ts
@@ -1,10 +1,11 @@
 import DataService from '../service/DataService';
+import PaisModel from '../db/models/pais';
 
 const router = (app: any, ds: DataService) => {
     app.route('/usuario')
         .get((req: any, res: any) => {
             const usuario: any = ds.dbModels.usuario;
-            usuario.findAll().then((usuarios: any[]) => {
+            usuario.findAll({ include: [ds.dbModels.pais, ds.dbModels.provincia] }).then((usuarios: any[]) => {
                 res.send(usuarios);
             });
         })
