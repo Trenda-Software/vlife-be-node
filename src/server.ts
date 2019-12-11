679b6be37c633228cbe3734647687fa92ecb9039
jhackGC <javierhack@gmail.com>
2019-12-11 17:32:56 -0300

    test heroku deploy

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 console.log('############# dbConfig: ', dbConfig);
 
 const ds = new DataService(dbConfig);
-ds.connect();
+// ds.connect();
 
 // add routes
 // siteRoutes(app, ds);
