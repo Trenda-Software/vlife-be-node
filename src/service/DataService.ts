4dcb554325f048c765b271817f6f9a6c2875195e
jhackGC <javierhack@gmail.com>
2019-12-11 18:00:21 -0300

    exit app when not connecting to db

--- src/service/DataService.ts
+++ src/service/DataService.ts
@@ -55,6 +55,7 @@ export default class DataService {
             })
             .catch((err: any) => {
                 console.error('Unable to connect to the database:', err);
+                throw err;
             });
     };
 
