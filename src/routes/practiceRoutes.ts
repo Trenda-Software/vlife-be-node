71c1c0cb3ab8a790fa92b14dfb70392314965727
Maca <marianoe@gmail.com>
2020-03-24 18:16:39 -0300

    Mayuscula en el select de practice

--- src/routes/practiceRoutes.ts
+++ src/routes/practiceRoutes.ts
@@ -29,12 +29,12 @@ const router = (app: any, ds: DataService) => {
                     const practicas = req.body.practicesid;
                     var clausula = "";
                     for (let practica in practicas) {
-                        clausula = clausula + "practiceID = " + practicas[practica] + " or ";
+                        clausula = clausula + "PracticeID = " + practicas[practica] + " or ";
                     }
                     clausula = clausula.slice(0, -4);
                     console.log("where :" + clausula);
 
-                    ds.dbClient.query("select professionals.id,name,surname,sum(cost) as cost, '1km' as distance from professionals  inner join practicecosts on professionals.id = professionalid where professionals.id in (select professionalid from practicecosts where " + clausula + " ) group by professionalid ", { type: Sequelize.QueryTypes.SELECT })
+                    ds.dbClient.query("select Professionals.id,name,surname,sum(cost) as cost, '1km' as distance from Professionals  inner join PracticeCosts on Professionals.id = Professionalid where Professionals.id in (select Professionalid from PracticeCosts where " + clausula + " ) group by Professionalid ", { type: Sequelize.QueryTypes.SELECT })
                         .then((profesional: any[]) => {
                             // We don't need spread here, since only the results will be returned for select queries
                             console.log(JSON.stringify(profesional));
